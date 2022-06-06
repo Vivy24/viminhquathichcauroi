0d153e41dfd6b27a2a607defe2e4da0171d61d87
Vivy Vuong <vngvy48@gmail.com>
2022-06-06 10:26:47 -0400

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const noButton = document.querySelector(".noButton");
 
 noButton.addEventListener("mouseover", () => {
-  const i = Math.floor(Math.random() * 500) + 1;
+  const i = Math.floor(Math.random() * 1200) + 1;
   const j = Math.floor(Math.random() * 500) + 1;
   noButton.style.display = "block";
   noButton.style.position = "absolute";
