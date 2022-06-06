be2ee64d687792649ebde1d0c82f3e5736242bb4
Vivy Vuong <vngvy48@gmail.com>
2022-06-06 10:04:18 -0400

    Finish

---
+++ script.js
@@ -0,0 +1,10 @@
+const noButton = document.querySelector(".noButton");
+
+noButton.addEventListener("mouseover", () => {
+  const i = Math.floor(Math.random() * 500) + 1;
+  const j = Math.floor(Math.random() * 500) + 1;
+  noButton.style.display = "block";
+  noButton.style.position = "absolute";
+  noButton.style.left = i + "px";
+  noButton.style.top = j + "px";
+});
